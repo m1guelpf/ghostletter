ab81b872eeaa140937cdb28a3f6b39ba5fc8fbc7
Miguel Piedrafita <soy@miguelpiedrafita.com>
2018-11-11 17:55:11 +0100

    Add relevant MailerLite methods

---
+++ mailerlite/index.js
@@ -0,0 +1,20 @@
+const client = require('./client');
+
+const subscribeUser = email => {
+    const group = process.env.MAILERLITE_GROUP
+    return client.Post(`groups/${group}/subscribers`, {
+        email: email
+    }).then(() => {
+        console.log(`Subscribed ${email} to the list`)
+    })
+}
+
+const unsubscribeUser = email => {
+    return client.Put(`subscribers/${email}`, {
+        type: 'unsubscribed'
+    }).then(() => {
+        console.log(`Unsubscribed ${email} from the list`)
+    })
+}
+
+module.exports = {subscribeUser, unsubscribeUser}
